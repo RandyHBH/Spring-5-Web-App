60a9194c2f81a98d0dbc25ca1e4b2e8b30537cde
Randy Hector Bartumeu Huergo <randy@aleph.engineering>
2018-11-20 23:20:01 -0100

    Add equal, hash code and toString methods

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring5webapp.Model;
+package guru.springframework.spring5webapp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -59,5 +59,30 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
     }
 }
